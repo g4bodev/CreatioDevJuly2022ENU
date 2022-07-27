3b7d1b114658602838ece7f742f464777c329559
g4bodev <gabriel.paredes@semantiq.com.mx>
2022-07-26 23:54:42 -0500

    Section Realty

---
+++ DevJuly22/Schemas/UsrRealty69ec7718Section/UsrRealty69ec7718Section.js
@@ -0,0 +1,8 @@
+define("UsrRealty69ec7718Section", [], function() {
+	return {
+		entitySchemaName: "UsrRealty",
+		details: /**SCHEMA_DETAILS*/{}/**SCHEMA_DETAILS*/,
+		diff: /**SCHEMA_DIFF*/[]/**SCHEMA_DIFF*/,
+		methods: {}
+	};
+});
