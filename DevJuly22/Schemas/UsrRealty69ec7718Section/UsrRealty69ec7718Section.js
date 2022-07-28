ee85d4c2b00b5be448351fdd4fab3d6385f385ed
g4bodev <gabriel.paredes@semantiq.com.mx>
2022-07-28 15:17:55 -0500

    Business rules combiding client side

--- DevJuly22/Schemas/UsrRealty69ec7718Section/UsrRealty69ec7718Section.js
+++
@@ -1,8 +0,0 @@
-define("UsrRealty69ec7718Section", [], function() {
-	return {
-		entitySchemaName: "UsrRealty",
-		details: /**SCHEMA_DETAILS*/{}/**SCHEMA_DETAILS*/,
-		diff: /**SCHEMA_DIFF*/[]/**SCHEMA_DIFF*/,
-		methods: {}
-	};
-});
