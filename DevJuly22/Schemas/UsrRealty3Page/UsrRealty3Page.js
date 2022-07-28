e82c91029740b831bc8630bf687d4a14f8ff8c40
g4bodev <gabriel.paredes@semantiq.com.mx>
2022-07-28 15:14:18 -0500

    Button action, business rules client side

--- DevJuly22/Schemas/UsrRealty3Page/UsrRealty3Page.js
+++ DevJuly22/Schemas/UsrRealty3Page/UsrRealty3Page.js
@@ -1,7 +1,13 @@
-define("UsrRealty3Page", [], function() {
+define("UsrRealty3Page", ["RightUtilities"], function(RightUtilities) {
 	return {
 		entitySchemaName: "UsrRealty",
-		attributes: {},
+		attributes: {
+			"CanEditPrice": {
+				dataValueType: Terrasoft.DataValueType.BOOLEAN,
+				type: Terrasoft.ViewModelColumnType.VIRTUAL_COLUMN,
+				value: false
+			}
+		},
 		modules: /**SCHEMA_MODULES*/{}/**SCHEMA_MODULES*/,
 		details: /**SCHEMA_DETAILS*/{
 			"Files": {
@@ -13,8 +19,94 @@ define("UsrRealty3Page", [], function() {
 				}
 			}
 		}/**SCHEMA_DETAILS*/,
-		businessRules: /**SCHEMA_BUSINESS_RULES*/{}/**SCHEMA_BUSINESS_RULES*/,
-		methods: {},
+		businessRules: /**SCHEMA_BUSINESS_RULES*/{
+			"UsrComment": {
+				"5c8e47ae-e7e1-4f6c-bfe3-a4672fbd4259": {
+					"uId": "5c8e47ae-e7e1-4f6c-bfe3-a4672fbd4259",
+					"enabled": true,
+					"removed": false,
+					"ruleType": 0,
+					"property": 2,
+					"logical": 0,
+					"conditions": [
+						{
+							"comparisonType": 7,
+							"leftExpression": {
+								"type": 1,
+								"attribute": "UsrPriceUSD"
+							},
+							"rightExpression": {
+								"type": 0,
+								"value": 1000000,
+								"dataValueType": 5
+							}
+						}
+					]
+				}
+			},
+			"UsrCity": {
+				"df4b6f69-2712-478f-9790-352b5eb5348f": {
+					"uId": "df4b6f69-2712-478f-9790-352b5eb5348f",
+					"enabled": true,
+					"removed": false,
+					"ruleType": 1,
+					"baseAttributePatch": "Country",
+					"comparisonType": 3,
+					"autoClean": true,
+					"autocomplete": true,
+					"type": 1,
+					"attribute": "UsrCountry"
+				}
+			},
+			"UsrPriceUSD": {
+				"d0b867b5-7c80-4c2f-a979-142a1e25efce": {
+					"uId": "d0b867b5-7c80-4c2f-a979-142a1e25efce",
+					"enabled": true,
+					"removed": false,
+					"ruleType": 0,
+					"property": 1,
+					"logical": 0,
+					"conditions": [
+						{
+							"comparisonType": 3,
+							"leftExpression": {
+								"type": 1,
+								"attribute": "CanEditPrice"
+							},
+							"rightExpression": {
+								"type": 0,
+								"value": true,
+								"dataValueType": 12
+							}
+						}
+					]
+				}
+			}
+		}/**SCHEMA_BUSINESS_RULES*/,
+		methods: {
+			onEntityInitialized: function(){
+				this.callParent(arguments);
+				this.setCanEditPriceAttribute();
+		    },
+			setCanEditPriceAttribute: function() {
+				RightUtilities.checkCanExecuteOperation({
+					operation: "CanChangeRealtyPrice"
+				}, this.getPriceOperationResult, this);
+			},
+			
+			getPriceOperationResult: function(result){
+				this.set("CanEditPrice", result);
+			},
+			onMyButtonClick: function() {
+				this.console.log("My button pressed.");
+				this.showInformationDialog("MY BUTTON was pressed!");
+			},
+			getMyButtonEnabled: function() {
+				var result = true;
+				//
+				return result;
+			}
+		},
 		dataModels: /**SCHEMA_DATA_MODELS*/{}/**SCHEMA_DATA_MODELS*/,
 		diff: /**SCHEMA_DIFF*/[
 			{
@@ -73,6 +165,33 @@ define("UsrRealty3Page", [], function() {
 			},
 			{
 				"operation": "insert",
+				"name": "MyButton",
+				"values": {
+					"layout": {
+						"colSpan": 24,
+						"rowSpan": 1,
+						"column": 0,
+						"row": 3,
+						"layoutName": "ProfileContainer"
+					},
+					"itemType": 5,
+					"caption": {
+						"bindTo": "Resources.Strings.MyButtonCaption"
+					},
+					"click": {
+						"bindTo": "onMyButtonClick"
+					},
+					"enabled": {
+						"bindTo": "getMyButtonEnabled"
+					},
+					"style": "blue"
+				},
+				"parentName": "ProfileContainer",
+				"propertyName": "items",
+				"index": 3
+			},
+			{
+				"operation": "insert",
 				"name": "LOOKUP0ec5f335-4998-4ba5-ac86-7559e16f147d",
 				"values": {
 					"layout": {
@@ -127,6 +246,40 @@ define("UsrRealty3Page", [], function() {
 				"parentName": "Header",
 				"propertyName": "items",
 				"index": 2
+			},
+			{
+				"operation": "insert",
+				"name": "UsrCountry753f2957-6c66-40ea-9235-4ca6927f18b7",
+				"values": {
+					"layout": {
+						"colSpan": 12,
+						"rowSpan": 1,
+						"column": 0,
+						"row": 3,
+						"layoutName": "Header"
+					},
+					"bindTo": "UsrCountry"
+				},
+				"parentName": "Header",
+				"propertyName": "items",
+				"index": 3
+			},
+			{
+				"operation": "insert",
+				"name": "UsrCity065ad773-ca4a-4c12-814a-90722f990675",
+				"values": {
+					"layout": {
+						"colSpan": 12,
+						"rowSpan": 1,
+						"column": 12,
+						"row": 3,
+						"layoutName": "Header"
+					},
+					"bindTo": "UsrCity"
+				},
+				"parentName": "Header",
+				"propertyName": "items",
+				"index": 4
 			},
 			{
 				"operation": "insert",
